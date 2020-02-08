10b69066ba903faf696024988f3929d4372ffcab
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2020-02-08 08:45:55 -0300

    added homescreen UI

--- src/php/home/home.php
+++ src/php/home/home.php
@@ -12,7 +12,7 @@ require_once '../session.php';
 
     <!-- local resources -->
     <link type="text/css" rel="stylesheet" href="../../lib/css/main.css" media="screen,projection" />
-    <link type="text/css" rel="stylesheet" href="../../lib/materialize/css/materialize.min.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="../../lib/materialize/css/materialize.css" media="screen,projection" />
 
     <!-- external libraries -->
     <!--Import Google Icon Font-->
@@ -20,14 +20,14 @@ require_once '../session.php';
     <!--Import materialize.css-->
 </head>
 
-<body>
+<body class="blue-grey darken-2">
     <ul id="dropdown1" class="dropdown-content">
         <!-- <li><a href="#!">one</a></li> -->
         <!-- <li><a href="#!">two</a></li> -->
         <!-- <li class="divider"></li> -->
         <li><a href="#!">Log Out</a></li>
     </ul>
-    <nav>
+    <nav class="col s8 offset-s4">
         <div class="nav-wrapper teal">
 
             <a href="#" class="brand-logo">Home</a>
@@ -36,7 +36,7 @@ require_once '../session.php';
                 <li>
                     <form>
                         <div class="input-field">
-                            <input id="search" type="search" required>
+                            <input id="search" class="white-text" type="search" required>
                             <label class="label-icon" for="search"><i class="material-icons">search</i></label>
                             <i class="material-icons">close</i>
                         </div>
@@ -47,11 +47,25 @@ require_once '../session.php';
             </ul>
         </div>
     </nav>
-
-    <div class="row">
-        <div class="col s8 offset-s2">
-            <table class="highlight responsive-table">
-                <thead>
+    <ul id="slide-out" class="sidenav sidenav-fixed blue-grey darken-4 white-text">
+        <li>
+            <div class="user-view">
+                <div class="background">
+                    <img src="../../lib/images/office.jpg">
+                </div>
+                <a href="#user"><img class="circle" src="../../lib/images/yuna.jpg"></a>
+                <a href="#name"><span class="white-text name">John Doe</span></a>
+                <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+            </div>
+        </li>
+        <li><a href="#!" class="white-text">First Sidebar Link</a></li>
+        <li><a href="#!" class="white-text">Second Sidebar Link</a></li>
+    </ul>
+    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <div class="row ">
+        <div class="col s9 offset-s3">
+            <table class="highlight responsive-table z-depth-4 white-text">
+                <thead class="blue-grey darken-4">
                     <tr>
                         <th>Naam</th>
                         <th>Omschrijving</th>
@@ -88,21 +102,21 @@ require_once '../session.php';
         </div>
     </div>
     <div class="fixed-action-btn">
-        <a class="btn-floating btn-large waves-effect waves-light red modal-trigger" href="#modal1"><i class="material-icons">add</i></a>
+        <a class="btn-floating btn-large waves-effect waves-light blue-grey darken-4 modal-trigger" href="#modal1"><i class="material-icons">add</i></a>
     </div>
     <form>
         <div id="modal1" class="modal col s6 offset-s3 modal">
             <!-- <div class="modal-content"> -->
             <div class="row">
-                <div class="input-field col s12">
+                <div class="input-field col s10 offset-s1">
                     <input placeholder="naam" id="naam" name="naam" type="text" class="validate white-text">
                     <label for="naam">Naam</label>
                 </div>
-                <div class="input-field col s12">
+                <div class="input-field col s10 offset-s1">
                     <input id="begin_datum" name="begin_datum" type="text" class="validate white-text datepicker ">
                     <label for="begin_datum">Begin Datum</label>
                 </div>
-                <div class="input-field col s12">
+                <div class="input-field col s10 offset-s1">
                     <input id="eind_datum" name="eind_datum" type="text" class="validate white-text datepicker ">
                     <label for="eind_datum"> Eind Datum</label>
                 </div>
