09575baac90803a97be525f4e37b4a26e10cccab
BR@5t0N <31829702+13A5T0N@users.noreply.github.com>
2020-05-26 02:46:08 -0300

    Update get_deelname_project_per_persoon.sql

--- src/php/requests/stats/get_deelname_project_per_persoon.sql
+++ src/php/requests/stats/get_deelname_project_per_persoon.sql
@@ -9,4 +9,4 @@ WHERE
     projecten.project_id = taken.project_id
         AND taken.taak_id = deelnemers_per_taak.taak_id
         AND deelnemers_per_taak.deelnemers_id = deelnemers.deelnemers_id
-        AND deelnemers_naam = 'Sabrina Starke'
+        AND deelnemers_naam = ''
