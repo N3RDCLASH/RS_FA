f472a658eef82fe801afc6a8692f4aa3add2e673
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2020-05-26 17:57:23 -0300

    updated sql file

--- src/php/requests/stats/get_deelname_project_per_persoon.sql
+++ src/php/requests/stats/get_deelname_project_per_persoon.sql
@@ -1,12 +1,16 @@
-SELECT 
-    deelnemers_naam, projecten.naam AS project, status
-FROM
-    deelnemers,
-    projecten,
-    taken,
-    deelnemers_per_taak
+SELECT
+  deelnemers_naam,
+  project.type,
+  projecten.naam AS project,
+  STATUS
+FROM deelnemers,
+  projecten,
+  taken,
+  deelnemers_per_taak
 WHERE
-    projecten.project_id = taken.project_id
-        AND taken.taak_id = deelnemers_per_taak.taak_id
-        AND deelnemers_per_taak.deelnemers_id = deelnemers.deelnemers_id
-        AND deelnemers_naam = ''
+  projecten.project_id = taken.project_id
+  AND taken.taak_id = deelnemers_per_taak.taak_id
+  AND deelnemers_per_taak.deelnemers_id = deelnemers.deelnemers_id
+  AND deelnemers.deelnemers_id =
+GROUP BY
+  project
